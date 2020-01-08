6ecdf76315eca879b88b65160e68afb17114a67f
pchel <ain.i@ngs.ru>
2020-01-08 21:03:50 +0700

    [z

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -3,10 +3,9 @@ package ru.stqa.pft.sandbox;
 import org.testng.annotations.Test;
 
 public class SquareTests {
-
   @Test
   public void testArea() {
-    Square s=new Square(5);
-    assert s.area()==24;
+    Square s = new Square(5);
+    assert s.area() == 24;
   }
 }
